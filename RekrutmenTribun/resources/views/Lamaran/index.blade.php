7553b08393a64cd46920f507b22c2b6dfdab89e7
robetbangunlegato <robetlegato22@mhs.mdp.ac.id>
2023-03-18 21:41:38 +0700

    lamaran index page

---
+++ RekrutmenTribun/resources/views/Lamaran/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 my-3">
+                    <h2 class="text-center">Selamat Datang di Website Rekrutmen, Silahkan Daftar.</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
--- RekrutmenTribun/routes/web.php
+++ RekrutmenTribun/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use  App\Http\Controllers\LandingController;
+use App\Http\Controllers\LamaranController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('landingpage', LandingController::class);
+Route::resource('lamaran', LamaranController::class);
